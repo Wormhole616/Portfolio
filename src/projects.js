366d4374886b3dd99274b0225467fcaaa5f40030
Wormhole616 <144727575+Wormhole616@users.noreply.github.com>
2024-05-02 22:16:46 -0700

    initial code

---
+++ src/projects.js
@@ -0,0 +1,59 @@
+// Array of projects to be rendered on portfolio page
+
+export const projects = [
+    {
+      projectTitle: "Work Day Planner",
+      description:
+        "A Useful tool for anyone who needs to keep track of their daily tasks. This application uses local storage to save your tasks.",
+      imageLink:
+        "https://github.com/Wormhole616/challenge05-planner/blob/master/images/Screenshot1.png",
+      liveLink: "https://wormhole616.github.io/challenge05-planner/",
+      gitHub: "https://github.com/Wormhole616/challenge05-planner",
+    },
+    {
+      projectTitle: "Project Horoscope",
+      description:
+        "A fun and interactive web application that allows users to get their daily horoscope and see how their day will go.",
+      imageLink:
+        "https://user-images.githubusercontent.com/120127903/250159501-c9b19908-7e37-480b-8126-3379d303a4ad.png",
+      liveLink: "https://wormhole616.github.io/ProjectHoroscope/",
+      gitHub: "https://github.com/Wormhole616/ProjectHoroscope",
+    },
+    {
+      projectTitle: "Scriptastic",
+      description:
+        "A Nice resourcefull Text Editor for developers. This application allows users to write, save, and delete code snippets.",
+      imageLink:
+        "https://github.com/Wormhole616/scripttastic/blob/main/screenshot/5.png",
+      liveLink: "https://scriptastic-bc6835a88150.herokuapp.com/",
+      gitHub: "https://github.com/Wormhole616/scripttastic",
+    },
+    {
+      projectTitle: "Client Portal",
+      description:
+        "A full stack MERN application that allows users to create an account upon approval and only by administrator, login, and view their profile. In addition the administrator has its own dashboard to manage users.",
+      imageLink:
+        "https://github.com/Wormhole616/GroupProject3/blob/main/image.png",
+      liveLink: "https://clientportal.onrender.com",
+      gitHub: "https://github.com/Wormhole616/GroupProject3",
+    },
+    {
+      projectTitle: "Weather-Teller",
+      description:
+        "Using third-party and server-side apis to bring clients live weather updates and future forecasts from anywhere in the world.",
+      imageLink:
+        "https://github.com/Wormhole616/Challenge05-Weather-teller/blob/main/assests/images/Screenshot%202024-01-29%20152733.png",
+      liveLink: "https://wormhole616.github.io/Challenge05-Weather-teller/",
+      gitHub: "https://github.com/Wormhole616/Challenge05-Weather-teller",
+    },
+    {
+      projectTitle: "My First Portfolio",
+      description:
+        "The first website I ever built. A simple, static website built with HTML and CSS.",
+      imageLink:
+        "https://github.com/Wormhole616/Portfolio-GDR/blob/main/assets/images/screenshot.png",
+      liveLink: "https://wormhole616.github.io/Portfolio-GDR/",
+      gitHub: "https://github.com/Wormhole616/Portfolio-GDR",
+    },
+    
+  ];
